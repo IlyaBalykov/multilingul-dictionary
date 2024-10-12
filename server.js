b0575fc974553671741f09288755fc8e3f3d726d
IlyaBalykov <balykov.ilya.88@gmail.com>
2024-10-13 00:13:18 +0300

    fix: repalce img & audio input tags with img & audio tags

--- server.js
+++ server.js
@@ -12,34 +12,8 @@ http.createServer(async (request, response) =>{
             buffer+=data
         }
 
-        //MySQL connect
         handler(buffer)
-        // const guatoValue = [`${guaWord}`, '-', '-', '-', '-', '-', '-', '-'];
-        // const sqlQuery = `INSERT INTO guato_dictionary.TRANSLATION (GUA, RUS, ENG, POR, TRANSCRIPTION, IMG_SRC, AUDIO_SRC, COMMENT) VALUES (?,?,?,?,?,?,?,?)`;
-        //
-        // const connection = mysql.createConnection(
-        //     {
-        //         host: 'localhost',
-        //         user: 'root',
-        //         database: 'guato_dictionary',
-        //         password: 'unstoppable'
-        //     }
-        // );
-        // connection.query(sqlQuery, guatoValue, function (err, results, fields) {
-        //     if(err){
-        //         console.log(err)
-        //     } else {
-        //         console.log(results)
-        //     } //data
-        //     // console.log(fields)  //meta data jf rows
-        // })
-        //
-        // connection.end(function (err) {
-        //     if(err){
-        //         return console.log('Error! ' + err.message)
-        //     }
-        //     console.log('Connection closed.')
-        // })
+
         response.end('Data received',()=>console.log('Response end'))
     } else {
         fs.readFile('index.html', (error, data) => {
