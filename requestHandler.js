0901e1315a75b9793db9c973886773a66693ed0f
IlyaBalykov <balykov.ilya.88@gmail.com>
2024-10-13 21:08:47 +0300

    feat: add some style changes for index.html

--- requestHandler.js
+++ requestHandler.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 module.exports = function postHandler(bodyForm) {
     const mysql = require('mysql2');
     let guatoValue = ['-', '-', '-', '-', '-', '-', '-', '-'];
@@ -12,7 +13,50 @@ module.exports = function postHandler(bodyForm) {
         }
     );
     // Handler of body form
+    console.log(bodyForm)
     const complexData = bodyForm.split('&')
+    console.log(complexData)
+    for (const parameter of complexData) {
+        const [parameterName, parameterValue] = parameter.split('=')
+        if(parameterName === "guaword") guatoValue[0] = parameterValue
+        if(parameterName === "guatranscription") guatoValue[1] = parameterValue
+        if(parameterName === "engtranslation") guatoValue[2] = parameterValue
+    }
+
+    connection.query(sqlQuery, guatoValue, function (err, results, fields) {
+        if(err){
+            console.log(err)
+        } else {
+            console.log(results)
+        } //data
+        // console.log(fields)  //meta data of rows
+    })
+
+    connection.end(function (err) {
+        if(err){
+            return console.log('Error! ' + err.message)
+        }
+        console.log('Connection closed.')
+    })
+}
+
+module.exports = function getHandler(getForm) {
+    const mysql = require('mysql2');
+    let guatoValue = ['-', '-', '-', '-', '-', '-', '-', '-'];
+    const sqlQuery = `SELECT * FROM guato_dictionary.TRANSLATION; WHERE `; // Дописать
+
+    const connection = mysql.createConnection(
+        {
+            host: 'localhost',
+            user: 'root',
+            database: 'guato_dictionary',
+            password: 'unstoppable'
+        }
+    );
+    // Handler of body form
+    console.log(bodyForm)
+    const complexData = bodyForm.split('&')
+    console.log(complexData)
     for (const parameter of complexData) {
         const [parameterName, parameterValue] = parameter.split('=')
         if(parameterName === "guaword") guatoValue[0] = parameterValue
